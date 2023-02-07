2e06b52f057b3579e34a9d8a608e044e1be0652b
Alex Dickinson <P215280@outlook.com>
2023-02-07 12:01:01 +0000

    task1
    not complete

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -7,58 +7,29 @@ namespace CMP1903MWorkshopCode
         static void Main(string[] args)
         {
             //Week 2
-            //EITHER:
-            //Read in the text file if you want (or are able) [HINT: read into a list of strings]
-            //Split the first four numbers
-            List<string> streets = new List<string>();
+            //Read in the items from streetCodes.txt
+            List<string> streetCodes = new List<string>();
+            streetCodes = System.IO.File.ReadAllLines(@"C:\Users\james\source\repos\CMP1903M Workshop Code\CMP1903M Workshop Code\streetCodes.txt").ToList();
 
-
-
-            //OR:
-            //Manual (check each street code individually without reading them in automatically)
-            //long, tedious, etc, etc
-            //7778[8] is a correct street code
             //Second two variables here are to use when calculating the checksum
-            int streetCode = 7778;
             int checkSum = 8;
             int total = 1;
             int total2 = 0;
-          
 
-            //Multiply all the numbers together in the 4 digit code (7*7*7*8)
-            //Can do it by using the mod '%' operator and the divide '/' operator
-            //Reduce streetCode by 1 digit each time through the loop
-            while(streetCode > 0)
+            //Loop through each item in the list
+            foreach (string streetCode in streetCodes)
             {
-                
+                //Convert streetcode to int
+                int streetCodeInt = Convert.ToInt32(streetCode.Substring(0,4));
 
-            }
+                //Get the checksum for streetCodeInt
+                int checkSum2 = streetCodeInt % checkSum;
 
-
-            //Add the digits of the multiplication total together
-            //infinite loop until the total gets to one digit - the checksum
-            //Use 'break;' to break out of the loop
-            while (true)
-            {
-                //Do the same as above, adding not multiplying
-                while (total > 0)
+                if (checkSum2 == Convert.ToInt32(streetCode.Substring(5, 1)))
                 {
-                    
-
+                    Console.WriteLine(streetCode);
                 }
-
-                //Check if total2 is 2 digits or more
-                //If not, break out of the loop
-                if (total2 > 9) 
-                { 
-                    
-                }
-                else { break; }
             }
-  
-            //Check if the final checksum is the same as the checksum in the street code.
-            
-
         }
     }
 }
